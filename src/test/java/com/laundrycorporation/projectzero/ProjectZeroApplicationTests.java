644baab2d2888857ed4b2c48a329f704cab6cbf4
iAwale <chiragawale777@gmail.com>
2023-02-24 11:04:34 -0500

    Initial commit

---
+++ src/test/java/com/laundrycorporation/projectzero/ProjectZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laundrycorporation.projectzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
